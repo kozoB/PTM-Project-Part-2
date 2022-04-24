9f2978ae91d869a0f1e0e9fcff1db6ce3662561c
KozoB <bar.koz3151@gmail.com>
2022-04-24 19:05:00 +0300

    Time Series by lines

--- PTM Project Part 2/src/test/SimpleAnomalyDetector.java
+++ PTM Project Part 2/src/test/SimpleAnomalyDetector.java
@@ -6,16 +6,22 @@ public class SimpleAnomalyDetector implements TimeSeriesAnomalyDetector {
 	
 
 	@Override
-	public void learnNormal(TimeSeries ts) {
+	public void learnNormal(TimeSeries ts)
+	{
+//		for() 
 	}
 
 
 	@Override
-	public List<AnomalyReport> detect(TimeSeries ts) {
+	public List<AnomalyReport> detect(TimeSeries ts)
+	{
+		
 		return null;
 	}
 	
-	public List<CorrelatedFeatures> getNormalModel(){
+	public List<CorrelatedFeatures> getNormalModel()
+	{
+		
 		return null;
 	}
 }
